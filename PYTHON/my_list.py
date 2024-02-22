e1491296562b6af8027bc1d9380d8dfcadeed6c1
lkendi <lkendi003@gmail.com>
2024-02-22 20:24:18 +0300

    Add python week 2 assignment

---
+++ PYTHON/my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list[1] = 15
+# extend the list
+my_list.extend([50, 60, 70])
+# remove the last element
+del(my_list[-1])
+# sort in ascending order
+my_list.sort()
+# find and print the index of 30
+print(my_list.index(30))
