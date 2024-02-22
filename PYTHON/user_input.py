173a3dc9c7970db4ca51668ca6f9e4a713d075a6
lkendi <lkendi003@gmail.com>
2024-02-22 20:04:18 +0300

    Add 1st python assignment

---
+++ PYTHON/user_input.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""User input Module"""
+name = input("What is your name? ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+print(f"Hello {name}, you are {age} years old and live in {location}.")
